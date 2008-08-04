953b58d0c31832cf3e78ecb0a84567ca6df6f358
David Joyner <david.joyner@spirent.com>
2008-08-04 15:59:24 -0400

    Turned off emacs p4-mode when running under Darwin

--- emacs/lisp/my-p4mode.el
+++ emacs/lisp/my-p4mode.el
@@ -1,7 +1,9 @@
 ;;; ~/emacs/lisp/my-p4mode.el
 
-(require 'p4)
-(setq p4-use-p4config-exclusively t)
+(when (not (eq system-type 'darwin))
+  (progn
+    (require 'p4)
+    (setq p4-use-p4config-exclusively t)))
 
 ;;; end ~/emacs/lisp/my-p4mode.el
 
